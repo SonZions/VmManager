46bef1d95ce974a841cb3ec91c10d948ea9d8bb8
SonZions <96846796+SonZions@users.noreply.github.com>
2025-04-28 22:50:43 +0200

    fix import

--- vm_webapp/app/main.py
+++ vm_webapp/app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request, RedirectResponse
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 import vm_manager
 
